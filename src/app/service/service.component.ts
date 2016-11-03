af818d0897a5bd19a8492fa2f9b1bef0d870a525
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-03 11:15:39 -0500

    Injected DataService in service.component.

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-service',
   template: `
     <app-cmp-a></app-cmp-a>
     <app-cmp-b></app-cmp-b>
   `,
-  styles: []
+  providers: [DataService]
 })
 export class ServiceComponent implements OnInit {
 
